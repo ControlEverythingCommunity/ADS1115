5906254fceb982ff6119d1df907b1165531945e2
Yadwinder Singh <ryker1990@gmail.com>
2016-07-13 22:39:29 +0530

    Update ADS1115.py

--- Python/ADS1115.py
+++ Python/ADS1115.py
@@ -11,9 +11,9 @@ import time
 bus = smbus.SMBus(1)
 
 # ADS1115 address, 0x48(72)
-# Select configuration register, 0x01(00)
+# Select configuration register, 0x01(01)
 #		0x8483(33923)	AINP = AIN0 and AINN = AIN1, +/- 2.048V
-#                       Continuous conversion mode, 128SPS
+#                 Continuous conversion mode, 128SPS
 data = [0x84,0x83]
 bus.write_i2c_block_data(0x48, 0x01, data)
 
