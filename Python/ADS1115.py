9add4edb1307ca87b10702c6f08d36db9dc0b794
Yadwinder Singh <ryker1990@gmail.com>
2016-11-03 16:11:23 +0530

    Update ADS1115.py

--- Python/ADS1115.py
+++ Python/ADS1115.py
@@ -13,7 +13,7 @@ bus = smbus.SMBus(1)
 # ADS1115 address, 0x48(72)
 # Select configuration register, 0x01(01)
 #		0x8483(33923)	AINP = AIN0 and AINN = AIN1, +/- 2.048V
-#                 Continuous conversion mode, 128SPS
+#				Continuous conversion mode, 128SPS
 data = [0x84,0x83]
 bus.write_i2c_block_data(0x48, 0x01, data)
 
@@ -26,6 +26,9 @@ data = bus.read_i2c_block_data(0x48, 0x00, 2)
 
 # Convert the data
 raw_adc = data[0] * 256 + data[1]
+
+if raw_adc > 32767:
+	raw_adc -= 65535
 
 # Output data to screen
 print "Digital Value of Analog Input : %d" %raw_adc
