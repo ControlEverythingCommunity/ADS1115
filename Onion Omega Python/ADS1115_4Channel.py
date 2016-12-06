d22517aa2727de7cc506717dc023758046340f31
Yadwinder Singh <ryker1990@gmail.com>
2016-12-06 17:13:01 +0530

    Update ADS1115_4Channel.py

--- Onion Omega Python/ADS1115_4Channel.py
+++ Onion Omega Python/ADS1115_4Channel.py
@@ -12,9 +12,9 @@ i2c = onionI2C.OnionI2C()
 
 # ADS1115 address, 0x48(72)
 # Select configuration register, 0x01(01)
-#		0x8483(33923)	AINP = AIN0 and AINN = AIN1, +/- 2.048V
-#						Continuous conversion mode, 128SPS
-data = [0x84,0x83]
+#		0xC483(50307)	AINP = AIN0 and AINN = GND, +/- 2.048V
+#				Continuous conversion mode, 128SPS
+data = [0xC4,0x83]
 i2c.writeBytes(0x48, 0x01, data)
 
 time.sleep(0.5)
@@ -31,13 +31,13 @@ if raw_adc > 32767:
 	raw_adc -= 65535
 
 # Output data to screen
-print "Digital Value of Analog Input on AIN0 & AIN1: %d" %raw_adc
+print "Digital Value of Analog Input on Channel-0: %d" %raw_adc
 
 # ADS1115 address, 0x48(72)
 # Select configuration register, 0x01(01)
-#		0x9483(38019)	AINP = AIN0 and AINN = AIN3, +/- 2.048V
-#						Continuous conversion mode, 128SPS
-data = [0x94,0x83]
+#		0xD483(54403)	AINP = AIN1 and AINN = GND, +/- 2.048V
+#				Continuous conversion mode, 128SPS
+data = [0xD4,0x83]
 i2c.writeBytes(0x48, 0x01, data)
 
 time.sleep(0.5)
@@ -54,13 +54,13 @@ if raw_adc > 32767:
 	raw_adc -= 65535
 
 # Output data to screen
-print "Digital Value of Analog Input on AIN0 & AIN3: %d" %raw_adc
+print "Digital Value of Analog Input on Channel-1: %d" %raw_adc
 
 # ADS1115 address, 0x48(72)
 # Select configuration register, 0x01(01)
-#		0xA483(42115)	AINP = AIN1 and AINN = AIN3, +/- 2.048V
-#						Continuous conversion mode, 128SPS
-data = [0xA4,0x83]
+#		0xE483(58499)	AINP = AIN2 and AINN = GND, +/- 2.048V
+#				Continuous conversion mode, 128SPS
+data = [0xE4,0x83]
 i2c.writeBytes(0x48, 0x01, data)
 
 time.sleep(0.5)
@@ -77,13 +77,13 @@ if raw_adc > 32767:
 	raw_adc -= 65535
 
 # Output data to screen
-print "Digital Value of Analog Input on AIN1 & AIN3: %d" %raw_adc
+print "Digital Value of Analog Input on Channel-2: %d" %raw_adc
 
 # ADS1115 address, 0x48(72)
 # Select configuration register, 0x01(01)
-#		0xB483(46211)	AINP = AIN2 and AINN = AIN3, +/- 2.048V
-#						Continuous conversion mode, 128SPS
-data = [0xB4,0x83]
+#		0xF483(62595)	AINP = AIN3 and AINN = GND, +/- 2.048V
+#				Continuous conversion mode, 128SPS
+data = [0xF4,0x83]
 i2c.writeBytes(0x48, 0x01, data)
 
 time.sleep(0.5)
@@ -100,4 +100,4 @@ if raw_adc > 32767:
 	raw_adc -= 65535
 
 # Output data to screen
-print "Digital Value of Analog Input on AIN2 & AIN3: %d" %raw_adc
+print "Digital Value of Analog Input on Channel-3: %d" %raw_adc
